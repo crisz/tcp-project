d387f7d5e0069599b3bd419109faa297c4b74fd6
liuzzom <mauro.liuzzo@hotmail.it>
2018-01-13 20:12:43 +0100

    =Fixed GetAllEmployee and setted all places in Location on the DB

--- src/it/metallicdonkey/tcp/db/DBHelper.java
+++ src/it/metallicdonkey/tcp/db/DBHelper.java
@@ -2,6 +2,8 @@ package it.metallicdonkey.tcp.db;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+
+import it.metallicdonkey.tcp.login.Role;
 import it.metallicdonkey.tcp.models.*;
 
 public class DBHelper {
@@ -20,10 +22,10 @@ public class DBHelper {
 		return instance;
 	}
 	
-	public static ArrayList<Employee> queryEmployee(String select, String where){
+	public static ArrayList<Employee> getAllEmployees(String clause){
 		ArrayList<Employee> employees = new ArrayList<>();
 		try {
-			dbm.executeQuery("SELECT "+select+" FROM employee WHERE "+where);
+			dbm.executeQuery("SELECT * FROM employee WHERE "+clause);
 			while(dbm.getResultSet().next()) {
 				Employee e= new Employee();
 				e.setId(dbm.getResultSet().getString("idEmployee"));
@@ -35,11 +37,12 @@ public class DBHelper {
 				e.setAddress(dbm.getResultSet().getString("Address"));
 				e.setSalary(dbm.getResultSet().getDouble("GrossSalary"));
 				String stringa = dbm.getResultSet().getString("Status");
-				//e.setStatus(dbm.getResultSet().getString("Status"));
-				//e.setRole(dbm.getResultSet().getString("Role"));
-				//e.setWorkshift(dbm.getResultSet().getString("Workshift"));
-				employees.add(e);
-				
+				e.setStatus(StatusEmployee.valueOf(stringa));
+				stringa = dbm.getResultSet().getString("Role");
+				e.setRole(Role.valueOf(stringa));
+				dbm.getResultSet().getString("Workshift");
+				e.setWorkshift(Workshift.valueOf(stringa));
+				employees.add(e);				
 			}
 		}
 		catch(SQLException exc) {
