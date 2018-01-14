bbc73f909bfe3eb1a87bd57d9fa5a470fecd6f30
GioGiglio <giovannimaria.giglio@gmail.com>
2018-01-14 23:00:39 +0100

    Added PaySalary Screen and Ctrl

--- src/it/metallicdonkey/tcp/HRArea/EmployeeDataModel.java
+++ src/it/metallicdonkey/tcp/HRArea/EmployeeDataModel.java
@@ -1,15 +1,32 @@
 package it.metallicdonkey.tcp.HRArea;
 
+
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class EmployeeDataModel {
 	private final SimpleStringProperty id;
 	private final SimpleStringProperty nome;
 	private final SimpleStringProperty cognome;
+	private final SimpleStringProperty role;
+	private final SimpleDoubleProperty salary;
+	
 	public EmployeeDataModel(String id, String nome, String cognome) {
 		this.id = new SimpleStringProperty(id);
 		this.nome = new SimpleStringProperty(nome);
 		this.cognome = new SimpleStringProperty(cognome);
+		this.role = null;
+		this.salary = null;
+	}
+	
+	public EmployeeDataModel(String id, String role, double salary) {
+		this.id = new SimpleStringProperty(id);
+		this.role = new SimpleStringProperty(role);
+		this.salary = new SimpleDoubleProperty(salary);
+		this.nome = null;
+		this.cognome = null;
 	}
 
 	public String getNomeECognome() {
@@ -23,5 +40,13 @@ public class EmployeeDataModel {
 	}
 	public String getLastName() {
 		return this.cognome.get();
+	}
+	
+	public String getRole() {
+		return this.role.get();
+	}
+	
+	public double getSalary() {
+		return this.salary.get();
 	}
 }
