60ce57a2ba5eb6d76c2c613b784f7ba7f4438deb
=Cristian Traina <crissstian96@gmail.com>
2018-01-16 13:52:11 +0100

    Added entry vehicle

--- src/it/metallicdonkey/tcp/login/LoginCtrl.java
+++ src/it/metallicdonkey/tcp/login/LoginCtrl.java
@@ -43,7 +43,7 @@ public class LoginCtrl {
 		e.setStatus(StatusEmployee.AVAILABLE);
 		e.setRole(Role.Autista);
 		e.setWorkshift(Workshift.MATTINA);
-		Home home = Home.getHome(Role.Addetto_agli_impiegati);
+		Home home = Home.getHome(Role.Addetto_ai_mezzi);
 		home.goHome(mainApp);
 
 //  	if(false) {
--- src/it/metallicdonkey/tcp/vehicleArea/EntryVehicleCtrl.java
+++ src/it/metallicdonkey/tcp/vehicleArea/EntryVehicleCtrl.java
@@ -1,12 +1,20 @@
 package it.metallicdonkey.tcp.vehicleArea;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import it.metallicdonkey.tcp.App;
+import it.metallicdonkey.tcp.db.DBHelper;
 import it.metallicdonkey.tcp.login.Home;
+import it.metallicdonkey.tcp.models.Location;
+import it.metallicdonkey.tcp.models.StatusVehicle;
+import it.metallicdonkey.tcp.models.Vehicle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class EntryVehicleCtrl {
 	private App mainApp;
@@ -22,12 +30,47 @@ public class EntryVehicleCtrl {
 	}
 
 	@FXML
-	private void submitVehicle() {
-		String result = "L'ingresso del veicolo " + vehicleField.getText() + " è avvenuto con successo. La postazione assegnata è la B241";
+	private void submitVehicle() throws SQLException {
+		ArrayList<Location> al = DBHelper.getInstance().getAllFreeLocations();
+		
+		System.out.println("al");
+
+		System.out.println(al.size());
+		Location l = al.get((int) Math.floor(Math.random() * al.size()));
+		int location = l.getId_Location();
+		
+		Vehicle v = null;
+		ArrayList<Vehicle> av = DBHelper.getInstance().getAllVehiclesArray();
+		
+		System.out.println(av);
+		for(int i=0; i<av.size(); i++) {
+			if(av.get(i).getId().equals(vehicleField.getText()))
+				v = av.get(i);
+		}
+		
+		if(v==null) {
+			Alert alert = new Alert(AlertType.WARNING);
+	    alert.initOwner(mainApp.getPrimaryStage());
+	    alert.setTitle("Avviso");
+	    alert.setHeaderText("Inserimento fallito!");
+	    alert.setContentText("Il veicolo con matricola " + vehicleField.getText() + " non esiste.");
+	    alert.showAndWait();
+	    return;
+		}
+		
+		if(v.getStatus() == StatusVehicle.AVAILABLE) {
+			Alert alert = new Alert(AlertType.WARNING);
+	    alert.initOwner(mainApp.getPrimaryStage());
+	    alert.setTitle("Avviso");
+	    alert.setHeaderText("Inserimento fallito!");
+	    alert.setContentText("Il veicolo con matricola " + vehicleField.getText() + " è già in deposito.");
+	    alert.showAndWait();
+	    return;
+		}
+
+		String result = "L'ingresso del veicolo " + vehicleField.getText() + " è avvenuto con successo. La postazione assegnata è: " + location;
 		resultLabel.setText(result);
-		/*
-		 * TODO: Sostituire con db
-		 */
+		
 	}
 
 	@FXML
